678b172181a5c012f80968aca6b99cfe11c6dad0
MaxSkaw <maximilian.skawran@fau.de>
2023-05-17 14:29:53 +0200

    GitIgnore 2

---
+++ data/pipeline.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+speed = pd.read_csv('speed.csv', sep=';', on_bad_lines='skip')
+weather = pd.read_csv('weather.csv', sep=',')
+
+speed.to_sql('speed', 'sqlite:///./data/speed.sqlite', if_exists='replace', index=False)
+weather.to_sql('weather', 'sqlite:///./data/weather.sqlite', if_exists='replace', index=False)
