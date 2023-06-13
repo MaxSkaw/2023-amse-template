24f1f1f99edf5182a5e3cce538682621e3263eb9
MaxSkaw <116753692+MaxSkaw@users.noreply.github.com>
2023-06-13 11:04:48 +0200

    Print Notes test.py

--- project/test.py
+++ project/test.py
@@ -11,6 +11,7 @@ if check_file(file_path):
     print("Success! The file was found here: " + file_path)
 else:
     print("Failure! The file was not found.")
+    print("Note: The file does not get uploaded to GitHub. Please keep in mind when checking from Actions.")
 
 # Testing for weather
 
@@ -20,3 +21,5 @@ if check_file(file_path):
     print("Success! The file was found here: " + file_path)
 else:
     print("Failure! The file was not found.")
+    print("Note: The file does not get uploaded to GitHub. Please keep in mind when checking from Actions.")
+    
