b2d915f400636d8676b63b7e3a846631837351e4
MaxSkaw <maximilian.skawran@fau.de>
2023-06-17 19:00:32 +0200

    Exercise 3.0

---
+++ exercises/exercise3.py
@@ -0,0 +1,53 @@
+import pandas as pd
+import sqlite3
+
+
+#----------- Data Source Management -----------#
+
+# Import dataset
+url = "https://www-genesis.destatis.de/genesis/downloads/00/tables/46251-0021_00.csv"
+df = pd.read_csv(url, sep=';', encoding="iso-8859-1", engine='python', skiprows=6, skipfooter=4, dtype={"Unnamed: 1": str})
+
+
+#----------- Data Manipulation  -----------#
+
+col_nums = {
+    0: "date",
+    1: "CIN",
+    2: "name",
+    12: "petrol",
+    22: "diesel",
+    33: "gas",
+    42: "electro",
+    52: "hybrid",
+    62: "plugInHybrid",
+    72: "others"
+}
+
+# Selection
+final_columns = [df.columns[col_idx] for col_idx in col_nums.keys()]
+df = df[final_columns]
+
+# Renaming
+df.columns = [col_nums[col_idx] for col_idx in col_nums.keys()]
+
+
+#----------- Data Cleanup  -----------#
+
+# Drop rows with missing values
+df = df.dropna()
+
+# CINs
+df = df[df["CIN"].str.len() == 5]
+
+# Positive integers
+numeric_cols = df.columns.difference(["date", "CIN", "name"])
+df[numeric_cols] = df[numeric_cols].apply(pd.to_numeric, errors="coerce")
+
+
+#----------- Data Export -----------#
+
+database = "cars.sqlite"
+conn = sqlite3.connect(database)
+df.to_sql("cars", conn, if_exists="replace", index=False)
+conn.close()
